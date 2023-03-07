0c01cffd38d26bb7a80381410ca3f2eceaf1cf3f
Silviu Oprea <silviu.oprea17@yahoo.com>
2023-03-07 23:51:41 +0200

    0006 - IV.The Roads & Intersections Project - do 2b, rework design model

--- RoadsIntersectionsProject/src/main/java/com/roadsintersections/Main.java
+++ RoadsIntersectionsProject/src/main/java/com/roadsintersections/Main.java
@@ -3,17 +3,27 @@ package com.roadsintersections;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        int value = 0;
         Set<Intersection> nodes = IntStream.rangeClosed(0, 8)
                 .mapToObj(i -> new Intersection("v" + i) )
                 .collect(Collectors.toSet());
-        List<Road> roads = IntStream.rangeClosed(0, 15)
+        LinkedList<Road> roads = IntStream.rangeClosed(0, 15)
                 .mapToObj(i -> new Road("e" + i, 2))
-                .collect(Collectors.toList());
+                .collect(Collectors.toCollection(LinkedList::new));
+        Intersection[] toAdd = nodes.toArray(new Intersection[0]);
+        roads.get(0).setIntersections(toAdd[0]);
+        roads.get(0).setIntersections(toAdd[1]);
+        roads.get(0).setIntersections(toAdd[2]);
+        roads.get(1).setIntersections(toAdd[0]);
+        roads.get(1).setIntersections(toAdd[2]);
+        roads.get(2).setIntersections(toAdd[0]);
+        roads.get(2).setIntersections(toAdd[3]);
         roads.get(7).setLength(1);
         roads.get(8).setLength(1);
         roads.get(9).setLength(1);
@@ -24,6 +34,11 @@ public class Main {
         roads.get(14).setLength(3);
         roads.get(15).setLength(3);
         roads.sort((Comparator.comparing(Road::getLength)));
+
+        List<Road> query = roads.stream()
+                .filter(r -> r.length > value)
+                .filter(r -> r.getIntersections().size() > 2)
+                .collect(Collectors.toCollection(ArrayList::new));
         System.out.println(roads.stream()
                 .map(Road::getLength)
                 .collect(Collectors.toList()));
@@ -32,6 +47,9 @@ public class Main {
                 .collect(Collectors.toSet()));
         for(Road road:roads)
             System.out.print("(" + road.getName() + " - " + road.getLength() + ") | ");
+        City iasi = new City("Iasi", nodes, roads);
         System.out.println();
+        for(Road road:query)
+            System.out.print("(" + road.getName() + " - " + road.getLength() + ") | ");
     }
 }
