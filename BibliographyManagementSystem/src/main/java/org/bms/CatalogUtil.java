33a19a44c38562098a85fc382945250543e8ffc6
Silviu Oprea <silviu.oprea17@yahoo.com>
2023-03-09 23:32:24 +0200

    0007 - V. Bibliography Management System - do point 1

---
+++ BibliographyManagementSystem/src/main/java/org/bms/CatalogUtil.java
@@ -0,0 +1,18 @@
+package org.bms;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CatalogUtil {
+    public static void save(Catalog catalog, String path) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(path), catalog);
+    }
+    public static Catalog load(String path) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new File(path), Catalog.class);
+    }
+
+}
