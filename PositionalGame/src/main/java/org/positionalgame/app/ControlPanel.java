6b7390a299872c43b1d81f620eb04a74ec45472d
Silviu Oprea <silviu.oprea17@yahoo.com>
2023-03-18 22:45:36 +0200

    0008 - VI. Positional Game - do point 2d

--- PositionalGame/src/main/java/org/positionalgame/app/ControlPanel.java
+++ PositionalGame/src/main/java/org/positionalgame/app/ControlPanel.java
@@ -1,8 +1,12 @@
 package org.positionalgame.app;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ControlPanel extends JPanel {
     final MainFrame frame;
@@ -28,5 +32,14 @@ public class ControlPanel extends JPanel {
     private void loadGame(ActionEvent e) {
     }
     private void saveGame(ActionEvent e) {
+        try
+        {
+            BufferedImage bi = frame.canvas.getImage();  // retrieve image
+            File outputFile = new File("saved.png");
+            ImageIO.write(bi, "png", outputFile);
+        }
+        catch (IOException b) {
+            System.err.println(b.getMessage());
+        }
     }
 }
